cca437fd151cd41a4c908e62750151e015767b8d
Alexander Shchukarev <aashchukarev@yandex.ru>
2021-11-24 18:25:52 +0300

    class of unit complited

---
+++ EmulatorOfFight.js
@@ -0,0 +1,17 @@
+'use strict';
+
+let player1 = [ { property1: 20, property2: 10 }, { property1: 15, property2: 15 } ];
+let player2 = [ { property1: 15, property2: 10 }, { property1: 15, property2: 15 } ];
+
+class EmulatorOfFight {
+    constructor( land, player1, player2 ) {
+        this.land = land;
+        this.player1 = player1;
+        this.player2 = player2;
+    }
+
+    chooseUnit( player1, player2 ) {
+        for( let unit of player1 )
+    }
+
+}
